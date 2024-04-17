72541eecebafa703d69b4dc64359af69a4a92dce
Khang Tran <ktran7246@gmail.com>
2024-04-16 22:35:42 -0500

    Image Loading and Shuffling Works. Added More Images

--- home/.config/nvim/lua/kenglordpanda/core/opts.lua
+++ home/.config/nvim/lua/kenglordpanda/core/opts.lua
@@ -5,7 +5,7 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 vim.opt.swapfile = false
 vim.opt.backup = false
 
