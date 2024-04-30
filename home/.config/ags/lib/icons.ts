06945e24b3f97198caa56fe0a21da1e1a5e37018
Khang Tran <ktran7246@gmail.com>
2024-04-30 17:04:18 -0500

    removed ags_khang, added use yaru-plus-git for good icon theme

--- home/.config/ags/lib/icons.ts
+++ home/.config/ags/lib/icons.ts
@@ -10,6 +10,7 @@ export const substitutes = {
     "preferences-system": "emblem-system-symbolic",
     "com.github.Aylur.ags-symbolic": "controls-symbolic",
     "com.github.Aylur.ags": "controls-symbolic",
+    "Alacritty": "terminal-symbolic"
 }
 
 export default {
